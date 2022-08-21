fc8e5d2c5a753621c24338cc2d4de192a36d138a
Vanessa <vanessamrios@gmail.com>
2022-08-21 17:38:19 +0200

    feat: implementa a funcão import_data que lê csv, json e xml na classe Inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xmltodict
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -20,12 +21,20 @@ class Inventory:
             products = json.loads(content)
         return products
 
+    def read_xml(path):
+        with open(path) as file:
+            content = xmltodict.parse(file.read())
+            products = content["dataset"]["record"]
+        return products
+
     def import_data(path, type):
 
         if path.endswith(".csv"):
             content = Inventory.read_csv(path)
         if path.endswith(".json"):
             content = Inventory.read_json(path)
+        if path.endswith(".xml"):
+            content = Inventory.read_xml(path)
 
         if type == "simples":
             return SimpleReport.generate(content)
