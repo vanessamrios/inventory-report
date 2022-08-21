b775917b29e432cfc54b52a5a521aefacae269e7
Vanessa <vanessamrios@gmail.com>
2022-08-21 17:13:32 +0200

    feat: implementa a funcão import_data que lê csv e json na classe Inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,11 +1,12 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
 
-    def read(path):
+    def read_csv(path):
         products = []
         with open(path) as file:
             content = csv.DictReader(file)
@@ -13,9 +14,18 @@ class Inventory:
                 products.append(line)
         return products
 
+    def read_json(path):
+        with open(path) as file:
+            content = file.read()
+            products = json.loads(content)
+        return products
+
     def import_data(path, type):
 
-        content = Inventory.read(path)
+        if path.endswith(".csv"):
+            content = Inventory.read_csv(path)
+        if path.endswith(".json"):
+            content = Inventory.read_json(path)
 
         if type == "simples":
             return SimpleReport.generate(content)
